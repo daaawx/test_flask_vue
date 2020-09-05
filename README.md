9f187a4796374a6aed04283fbec5e5db7dc62002
Dax <daaawx@gmail.com>
2020-09-05 23:53:53 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test Flask Vue
+
+An example Vue + Nuxt and Flask application.
+
+Special thanks to https://github.com/based-jace 👍
---
+++ api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/greeting')
+def greeting():
+    return {
+        'greeting': 'Hello from Flask!'
+    }
+
+
+if __name__ == '__main__':
+    app.run()
